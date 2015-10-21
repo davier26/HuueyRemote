e38d0891ff9f2c60576d2a5609388f1610256c1c
Nicholas Young <ynickd@gmail.com>
2015-10-21 14:15:41 -0700

    Initial Commit

---
+++ HuueyRemote/HuueyRemote.h
@@ -0,0 +1,19 @@
+//
+//  HuueyRemote.h
+//  HuueyRemote
+//
+//  Created by Nicholas Young on 10/21/15.
+//  Copyright © 2015 Nicholas Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HuueyRemote.
+FOUNDATION_EXPORT double HuueyRemoteVersionNumber;
+
+//! Project version string for HuueyRemote.
+FOUNDATION_EXPORT const unsigned char HuueyRemoteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HuueyRemote/PublicHeader.h>
+
+
